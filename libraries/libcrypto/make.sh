d94877f704abde4c19324f60edadcfa543b98dcc
Jörg F. Wittenberger <Joerg.Wittenberger@softeyes.net>
2020-07-30 12:59:54 +0200

    OPENSSL: security update to version 1.1.1g

--- libraries/libcrypto/make.sh
+++ libraries/libcrypto/make.sh
@@ -1,5 +1,7 @@
-PKGURL=https://www.openssl.org/source/openssl-1.1.1c.tar.gz
-PKGHASH=71b830a077276cbeccc994369538617a21bee808
+#PKGURL=https://www.openssl.org/source/openssl-1.1.1d.tar.gz
+#PKGHASH=056057782325134b76d1931c48f2c7e6595d7ef4
+PKGURL=https://www.openssl.org/source/openssl-1.1.1g.tar.gz
+PKGHASH=b213a293f2127ec3e323fb3cfc0c9807664fd997
 
 package_download $PKGURL $PKGHASH
 
@@ -33,12 +35,11 @@ macosx)
   fi
 ;;
 android*)
-
     EXTRACONF="android-arm -D__ANDROID_API__=$SYS_ANDROIDAPI"
     XX_ANDROID_NDK_HOME=`find ${SYS_PREFIX}/*-${SYS_ANDROIDAPI}-* -name AndroidVersion.txt 2> /dev/null`
     if [ "X$XX_ANDROID_NDK_HOME" = "X" ]; then
-      XX_ANDROID_NDK_HOME=`find $ANDROIDNDK -name AndroidVersion.txt 2> /dev/null`
-      /bin/cp $XX_ANDROID_NDK_HOME $SYS_PREFIX/android-ndk-*-toolchain
+      XX_ANDROID_NDK_HOME=`find $ANDROIDNDK/ -name AndroidVersion.txt 2> /dev/null`
+      /bin/cp "$XX_ANDROID_NDK_HOME" $SYS_PREFIX/android-ndk-*-toolchain
       XX_ANDROID_NDK_HOME=`find $SYS_PREFIX -name AndroidVersion.txt 2> /dev/null`
     fi
     export ANDROID_NDK_HOME=`dirname $XX_ANDROID_NDK_HOME`
@@ -54,11 +55,11 @@ case $SYS_PLATFORM in
     # `package_configure` from lambdanative when cross compiling for
     # Android.
     android)
-	./Configure --openssldir="$SYS_PREFIX" no-shared no-threads no-zlib no-asm no-dso no-sse2 $EXTRACONF # CC=$SYS_CC
+	./Configure --openssldir="$SYS_PREFIX" no-shared no-zlib no-asm no-dso no-sse2 $EXTRACONF || exit 1 # CC=$SYS_CC
     ;;
     *)
 	cp Configure configure   # openssl ships a Configure script
-	NOQUIET=yes package_configure --openssldir="$SYS_PREFIX" no-shared no-threads no-zlib no-asm no-dso no-sse2 $EXTRACONF # CC=$SYS_CC
+	NOQUIET=yes package_configure --openssldir="$SYS_PREFIX" no-shared no-zlib no-asm no-dso no-sse2 $EXTRACONF # CC=$SYS_CC
 ;;
 esac
 package_make build_libs
